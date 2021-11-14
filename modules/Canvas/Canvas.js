3e87d102313cc5defbf920e9d071530358c0441c
diego <ogedi97colombo@hotmail.it>
2021-11-15 00:00:22 +0100

    Edit the printing of the canvas

--- modules/Canvas/Canvas.js
+++ modules/Canvas/Canvas.js
@@ -1,14 +1,33 @@
 class Canvas {
     constructor() {
-
+        this.separation = 40;
     }
 
     draw() {
         //redraw graph with the new element
     }
 
+
+    /** Find positions of all currently placed elements and add offset for new position */
+    newPosition() {
+        let pos = [], found = false,
+        container = $('.container').offset().left;
+        let max = container;
+
+        $('.element').each(function() {
+            max = Math.max(max, $(this).offset().left);
+            found = true
+        });
+
+
+//        console.log(pos, container, max, found)
+        return max - container + (found ? this.separation : 0);
+    }
+
+
     addElement(elementInput) {
-        let html = `<div class="element" data-id="`+elementInput.ID+`"></div>`;
+        let style = 'style="left:'+ this.newPosition().toString() +'px"';
+        let html = `<div class="element" data-id="`+elementInput.ID+`" `+ style +`></div>`;
         $('.container').append(html);
         $(`.element[data-id='`+elementInput.ID+`']`).click((e) => {
             p.elementInput.open(parseInt(e.currentTarget.dataset['id']));
